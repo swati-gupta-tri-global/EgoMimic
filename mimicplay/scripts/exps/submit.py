d2e0cb17df8410341d682be657a2eb2245364a09
Pranay Mathur <pmathur39@sky2.cc.gatech.edu>
2024-02-12 11:49:08 -0500

    updated submit.py

--- mimicplay/scripts/exps/submit.py
+++ mimicplay/scripts/exps/submit.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # The specified folder is used to dump job information, logs and result when finished
     # %j is replaced by the job id at runtime
     assert args.name is not None, "Must provide a name for the experiment"
-    log_folder = f"/coc/flash9/skareer6/Projects/EgoPlay/EgoPlay/mimicplay/scripts/exps/{args.name}/%j"
+    log_folder = f"/coc/flash7/pmathur39/EgoPlay/mimicplay/scripts/exps/{args.name}/%j"
     executor = submitit.AutoExecutor(folder=log_folder)
     # The AutoExecutor provides a simple abstraction over SLURM to simplify switching between local and slurm jobs (or other clusters if plugins are available).
     # specify sbatch parameters (here it will timeout after 4min, and run on dev)
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # Cluster specific options must be appended by the cluster name:
     # Eg.: slurm partition can be specified using `slurm_partition` argument. It
     # will be ignored on other clusters:
-    executor.update_parameters(slurm_partition="hoffman-lab", cpus_per_task=13, nodes=1, slurm_ntasks_per_node=1, gpus_per_node="a40:1", slurm_qos="short", slurm_mem_per_gpu="40G", timeout_min=60*24*2)
+    executor.update_parameters(slurm_partition="rl2-lab", cpus_per_task=13, nodes=1, slurm_ntasks_per_node=1, gpus_per_node="a40:1", slurm_qos="short", slurm_mem_per_gpu="40G", timeout_min=60*24*2)
     # The submission interface is identical to concurrent.futures.Executor
 
     job = executor.submit(main, args)
