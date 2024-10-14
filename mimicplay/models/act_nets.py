8e077935a635e10d4dd6632d95ce5a6fa64845ab
Ryan Punamiya <ryanpunamiya@gmail.com>
2024-10-14 14:58:26 -0400

    fixed style encoder with extra layer norm

--- mimicplay/models/act_nets.py
+++ mimicplay/models/act_nets.py
@@ -7,90 +7,6 @@ from robomimic.models.transformers import PositionalEncoding
 from torch.distributions import Normal
 
 
-class PositionalEncoding2D(nn.Module):
-    """
-    This is a more standard version of the position embedding, very similar to the one
-    used by the Attention is all you need paper, generalized to work on images.
-
-    Copied from DETR repo; can be heavily optimized
-
-    Args:
-        num_pos_feats: hidden dimension (I think)
-        temperature: ???
-        normalize: whether to normalize embeddings
-        scale: used for normalization
-    """
-
-    def __init__(
-        self,
-        num_pos_feats: int = 64,
-        temperature: float = 10000,
-        normalize: bool = False,
-        scale: Optional[float] = None,
-    ):
-        super(PositionalEncoding2D, self).__init__()
-        self.num_pos_feats = num_pos_feats
-        self.temperature = temperature
-        self.normalize = normalize
-        if scale is not None and normalize is False:
-            raise ValueError("normalize should be True if scale is passed")
-        if scale is None:
-            scale = 2 * math.pi
-        self.scale = scale
-
-    def forward(self, x):
-        # x must be (bsz, ..., hidden_dim, H, W)
-        not_mask = torch.ones(x.shape[-2:])[None].to(x.device)
-        y_embed = not_mask.cumsum(1, dtype=torch.float32)
-        x_embed = not_mask.cumsum(2, dtype=torch.float32)
-        if self.normalize:
-            eps = 1e-6
-            y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale
-            x_embed = x_embed / (x_embed[:, :, -1:] + eps) * self.scale
-
-        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
-        dim_t = self.temperature ** (2 * (dim_t // 2) / self.num_pos_feats)
-
-        pos_x = x_embed[:, :, :, None] / dim_t
-        pos_y = y_embed[:, :, :, None] / dim_t
-        pos_x = torch.stack(
-            (pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4
-        ).flatten(3)
-        pos_y = torch.stack(
-            (pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4
-        ).flatten(3)
-        pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
-        return x + pos
-
-
-class LearnablePosEncoding2D(nn.Module):
-    """Learnable positional encoding for 2D inputs; inspired by DETR paper"""
-
-    def __init__(
-        self,
-        hidden_dim: int,
-        num_pos_feats: int = 64,
-        scale: float = 1.0,
-    ):
-        assert hidden_dim % 2 == 0, "Hidden dimension must be even"
-        super().__init__()
-        self.num_pos_feats = num_pos_feats
-        self.scale = scale
-        self.row_embed = nn.Parameter(torch.rand(num_pos_feats, hidden_dim // 2))
-        self.col_embed = nn.Parameter(torch.rand(num_pos_feats, hidden_dim // 2))
-
-    def forward(self, x):
-        H, W = x.shape[-2:]
-        col = self.col_embed[:W].unsqueeze(0).repeat(H, 1, 1)
-        row = self.row_embed[:H].unsqueeze(1).repeat(1, W, 1)
-        pos = torch.cat([col, row], dim=-1).flatten(0, 1).unsqueeze(1)
-        if x.dim() == 5:
-            num_imgs = x.shape[1]
-            pos = pos.repeat(num_imgs, 1, 1)
-            x = x.transpose(1, 2)
-        x = x.flatten(2).permute(2, 0, 1)
-        return self.scale * x + pos
-
 class Transformer(nn.Module):
     '''
     Basic transformer implementation using torch.nn. Also added option for custom pos embeddings. 
@@ -221,6 +137,8 @@ class StyleEncoder(nn.Module):
         self.latent_projection = nn.Linear(hidden_dim, latent_dim * 2)
 
         self.pos_encoding = PositionalEncoding(hidden_dim)
+
+        self.encoder_norm = nn.LayerNorm(hidden_dim)
     
     def forward(self, qpos, actions):
         bsz = qpos.shape[0]
@@ -241,6 +159,8 @@ class StyleEncoder(nn.Module):
         x = x.transpose(0, 1)  # [act_len + 2, bsz, hidden_dim]
 
         x = self.encoder(x)  # [act_len + 2, bsz, hidden_dim]
+
+        x = self.encoder_norm(x) # cuz act has weird pre and post norms
 
         x = x[0]  # [bsz, hidden_dim]
 
